2ae7d363097235cb8f04dfaf4dc7b3f237eedd22
ankur20us <ankur20us@users.noreply.github.com>
2019-02-19 15:36:24 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# How to use
+
+- npm install
+- npm run check
+- npm run build
+
+We have used a module called npx in order to call the reference modules, so please install **npx** beforehand globally using:
+
+- npm install -g npx
+
+1. **npm run check**
+	This checks against the possible wrong references with respect to the datatypes you mentioned.
+1. **npm run build**
+	This will remove all the refrences of flow and make the code exectuable since type refrences are not possible in **.js** files.
---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* @flow */
+'use strict';
+
+// `x` is declared to be a `number` explicitly.
+// This means Flow will throw an error if we try to assign
+// any value to `x` which is not a `number`.
+var x: number = 5;
+
+// this will generate the error, since you are pushing string to number
+x = 1234;
+
+// #FlowExpectError – string does not match type of x, which is number
+x = 'five';
+
+// If we don't declare variable type explicitly, it's not sealed.
+var y = true;
+
+y = false;
+
+// We are free to change the type any time by assigning a new value to the variable
+y = 'hello';
+
+// #FlowExpectError – Flow still type checks `y`, it knows which type it is currently.
+var z: boolean = y;
